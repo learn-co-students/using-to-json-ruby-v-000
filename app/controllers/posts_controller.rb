eaf7b53770257dc55d3c05961a81cfbf1fb968cf
Scott C. Reynolds <scott@scottcreynolds.com>
2016-02-22 11:36:42 -0500

    first draft

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
-    end
   end
 
   def new
@@ -34,9 +28,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    #render json: PostSerializer.serialize(post)
-    render json: post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}])
+    render json: PostSerializer.serialize(post)
   end
 
 private
