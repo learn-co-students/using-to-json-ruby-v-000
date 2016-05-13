4e5b1a9acf0d6bc78638d8818e3fe317b765865e
jaeku <ja.ku@rutgers.edu>
2016-05-13 17:05:28 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,13 @@ class PostsController < ApplicationController
   end
 
   def show
-  end
+      @post = Post.find(params[:id])
+      respond_to do |format|
+        format.html { render :show }
+        format.json { render json: @post.to_json(only: [:title, :description, :id],
+                                include: [author: { only: [:name]}]) }
+      end
+    end
 
   def new
     @post = Post.new
@@ -28,7 +34,8 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id],
+                              include: [ author: { only: [:name]}])
   end
 
 private
