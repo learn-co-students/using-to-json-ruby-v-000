97011a5f5c83f052676ca2475f98bd449f8b7184
Yechiel K <chilikkal@gmail.com>
2017-01-19 01:00:00 -0500

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,12 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    repond_to do |format|
+      format.html {render :show}
+      format.json {render json: post.to_json(only: [:title, :description, :id],
+                                include: [author: {only: [:name]}])}
+    end
   end
 
   def new
@@ -26,10 +32,6 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def post_data
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
 
 private
   # Use callbacks to share common setup or constraints between actions.
