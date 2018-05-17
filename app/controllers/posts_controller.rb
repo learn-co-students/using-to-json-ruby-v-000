2901e5d105cf1f9ee1eac8480230dfd9b9fa9251
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-17 01:16:37 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,9 +27,9 @@ class PostsController < ApplicationController
   end
 
   def post_data
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
+     post = Post.find(params[:id])
+     render json: post.to_json(include: :author)
+   end
 
 private
   # Use callbacks to share common setup or constraints between actions.
