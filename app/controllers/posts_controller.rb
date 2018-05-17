06596400636bed06d270bd3dd8df3336c9c5ba31
btcline04 <btcline04@gmail.com>
2018-05-16 20:45:13 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json
   end
 
 private
