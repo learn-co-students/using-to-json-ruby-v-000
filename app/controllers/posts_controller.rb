be929a1a726ccde1fcedd66f3531ea31515ecea1
remclay <remmy.lauren@gmail.com>
2018-05-21 22:58:03 +1000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,8 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @post.to_json(only: [:title, :description, :id],
-      include: [author: { only: [:name]}]) }
+        include: [author: { only: [:name]}])
+      }
     end
   end
 
