33b618f3aff0643d76cc363ef3f27e871a39d86a
Suchitra Naik <susanaik@gmail.com>
2018-10-02 14:05:30 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    byebug
+
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @post.to_json(only: [:title, :description, :id],
