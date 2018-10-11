8c12231194da9462684bd13f0c4426a0efaeafb2
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-11 13:42:39 -0400

    only

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: post.to_json(include: :author)
+   render json: post.to_json(only: [:title, :description, :id], include: [ author: { only: [:name]}])
   end
 
   private
