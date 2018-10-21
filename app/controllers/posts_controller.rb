4d59f514a12081ea3df78d0e2a6060e928de0a6d
Jul Jen <juliajen.prog@gmail.com>
2018-10-21 17:51:27 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,13 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post }
+    end
+  end
 
   def new
     @post = Post.new
@@ -13,20 +19,24 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    @post.save
-    redirect_to post_path(@post)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
-  def edit; end
+  def edit
+  end
 
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
-  def post_data
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+  def data
+    @post = Post.find(params[:id])
+    render json: PostSerializer.serialize(@post)
   end
 
   private
