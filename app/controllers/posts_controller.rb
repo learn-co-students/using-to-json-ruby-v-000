f8a1cbdbb71a318612115d8ab59964276dc5fd48
monksandninjas <blackbeltdiaries@gmail.com>
2019-01-12 02:57:48 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,16 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def edit; end
+  def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}]) }
+    end
+  end
+
+  def edit
+  end
 
   def update
     @post.update(post_params)
@@ -26,7 +35,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id], include: [ author: { only: [:name]}])
   end
 
   private
