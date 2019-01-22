9d77e3b35237733d6b4d2d4fa92bf0e2cb031521
github username <github email address>
2019-01-22 12:21:32 -0500

    Done.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,16 +1,16 @@
-class PostSerializer
-  def self.serialize(post)
-
-    # start with the open brace to create a valid JSON object
-    serialized_post = '{'
-
-    serialized_post += '"id": ' + post.id.to_s + ', '
-    serialized_post += '"title": "' + post.title + '", '
-    serialized_post += '"description": "' + post.description + '", '
-    serialized_post += '"author": {'
-    serialized_post += '"name": "' + post.author.name + '"}'
-
-    # and end with the close brace
-    serialized_post += '}'
-  end
-end
+# class PostSerializer
+#   def self.serialize(post)
+#
+#     # start with the open brace to create a valid JSON object
+#     serialized_post = '{'
+#
+#     serialized_post += '"id": ' + post.id.to_s + ', '
+#     serialized_post += '"title": "' + post.title + '", '
+#     serialized_post += '"description": "' + post.description + '", '
+#     serialized_post += '"author": {'
+#     serialized_post += '"name": "' + post.author.name + '"}'
+#
+#     # and end with the close brace
+#     serialized_post += '}'
+#   end
+# end
