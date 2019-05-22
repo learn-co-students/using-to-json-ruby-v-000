d3d456c700ce2754a379e1abaabe524bbdbc2cc9
Guess <guess@Guesss-iMac.local>
2019-05-21 21:46:50 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json(include: :author)
   end
 
   private
