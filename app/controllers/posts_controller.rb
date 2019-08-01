f319fad77499a535fb251d843f3d3590f6c27045
Mohammed Noori <kmanoorib@gmail.com>
2019-08-01 03:32:48 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,15 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def show; end
+  # def show; end
+    def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}]) }
+    end
+  end
 
   def new
     @post = Post.new
@@ -24,9 +32,16 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  #def post_data
+  #  post = Post.find(params[:id])
+  #  render json: PostSerializer.serialize(post)
+  #end
+  # from lecture
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    #render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id],
+                              include: [ author: { only: [:name]}])
   end
 
   private
